1b4e60d7f7998f8feb48449d508f3ee8cd66b090
chandrajeet kachhva <160776784+Chandrajeetkachhva0207@users.noreply.github.com>
2026-03-17 22:10:34 +0530

    Add multiple function definitions in 18-function.py

---
+++ 18-function.py
@@ -0,0 +1,62 @@
+# def greet():
+#     print("hello programers..")
+#     print("learning function")
+# greet()
+
+# def welcome(name):
+#     print("welcome:",name)
+
+# welcome("satish")
+
+# def add(a,b):
+#     print("total :",a+b)
+# add(3,2)  
+
+# def sub(a,b):
+#     print("subctration",a-b)
+# sub(2,1)
+
+# def add(a,b):
+#     return a+b
+
+# def mul(x):
+#     return x*2
+# result=mul(add(10,20))
+# print(result)
+
+
+#clean the text
+# def clean_text(value):
+#     return value.strip().title()
+
+# output=clean_text("   mUmBAi")
+# print(output)
+
+# def fix_city(city):
+#     city=city.lower().strip()
+#     city=city.replace("mombai","mumbai")
+#     city=city.replace("kolkat","kolkata")
+#     return city.title()
+
+# print(fix_city("mombai"))
+# print(fix_city("kolkat"))
+
+# def get_year(code):
+#     return code[-4:]
+# print(get_year("laptop-2024"))
+
+
+# def valid_email(email):
+#  return "@" in email and "." in email
+
+# print(valid_email("test@gmail.com"))
+
+# def total_salary(basic,bonus):
+#     return basic+bonus
+# print(total_salary(20000,50000))
+
+# def stats(nums):
+#     return min(nums),max(nums),sum(nums)/len(nums)
+# print(stats([10,20,30]))
+
+#
